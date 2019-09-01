d478cee1430fbe8aa78af6dc2de89dc6ebf6fd47
Maciej Mensfeld <maciej@mensfeld.pl>
2019-09-01 18:05:45 +0200

    default hooks off

--- lib/capistrano/coditsu.rb
+++ lib/capistrano/coditsu.rb
@@ -14,6 +14,8 @@ module Capistrano
     # Default values for Karafka settings
     def set_defaults
       set :bundle_clean_options, '--force'
+      set :puma_default_hooks, false
+      set :sidekiq_default_hooks, false
     end
   end
 end
@@ -22,4 +24,10 @@ end
 Capistrano::Coditsu::CAP_FILES.each do |cap_file|
   eval File.open(File.expand_path("../tasks/#{cap_file}.cap", __FILE__), 'r').read
 end
+
+before 'deploy:starting', :check_hooks do
+  invoke 'puma:add_default_hooks' if fetch(:puma_default_hooks)
+  invoke 'sidekiq:add_default_hooks' if fetch(:sidekiq_default_hooks)
+end
+
 # rubocop:enable Security/Eval
